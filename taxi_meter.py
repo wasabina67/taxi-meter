b2da5f4452ab992e44d0cda56a034f5ed9137fac
wasabina67 <wasabina67@gmail.com>
2026-04-29 14:14:46 +0900

    Update taxi_meter.py

--- taxi_meter.py
+++ taxi_meter.py
@@ -7,8 +7,23 @@ class TaxiMeter:
         self.total_time_low_speed = 0
         self.fare = base_fare
 
-    def update(self):
-        return
+    def _update_distance_fare(self, distance_km):
+        base_distance = 1.0
+        distance_step = 0.3
+        distance_fare = 100
+
+        prev_distance = self.total_distance
+        self.total_distance += distance_km
+
+        if self.total_distance > base_distance:
+            extra_distance = self.total_distance - base_distance
+            prev_extra_distance = max(0, prev_distance - base_distance)
+
+            added_steps = int(extra_distance / distance_step) - int(prev_extra_distance / distance_step)
+            self.fare += added_steps * distance_fare
+
+    def update(self, distance_km):
+        self._update_distance_fare(distance_km)
 
     def get_fare(self):
         return self.fare
