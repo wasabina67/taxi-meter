811630fe4203533cce96e2530889dfb603cb8857
wasabina67 <wasabina67@gmail.com>
2026-04-29 14:22:52 +0900

    Update taxi_meter.py

--- taxi_meter.py
+++ taxi_meter.py
@@ -7,6 +7,18 @@ class TaxiMeter:
         self.total_time_low_speed = 0
         self.fare = base_fare
 
+    def _update_time_fare(self, speed_kmh, elapsed_sec):
+        low_speed_threshold = 10
+        time_step = 90
+        time_fare = 100
+
+        if speed_kmh <= low_speed_threshold:
+            prev_time = self.total_time_low_speed
+            self.total_time_low_speed += elapsed_sec
+
+            added_steps = int(self.total_time_low_speed / time_step) - int(prev_time / time_step)
+            self.fare += added_steps * time_fare
+
     def _update_distance_fare(self, distance_km):
         base_distance = 1.0
         distance_step = 0.3
@@ -22,8 +34,9 @@ class TaxiMeter:
             added_steps = int(extra_distance / distance_step) - int(prev_extra_distance / distance_step)
             self.fare += added_steps * distance_fare
 
-    def update(self, distance_km):
+    def update(self, distance_km, speed_kmh, elapsed_sec):
         self._update_distance_fare(distance_km)
+        self._update_time_fare(speed_kmh, elapsed_sec)
 
     def get_fare(self):
         return self.fare
